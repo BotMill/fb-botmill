15e589ee8a3df03ccb1ab96dc0bc206b815c8eb6
Donato Rimenti <donatohan.rimenti@gmail.com>
2016-11-25 01:12:48 +0100

    Added Receipt template

---
+++ src/main/java/co/aurasphere/facebot/model/outcoming/template/receipt/Adjustment.java
@@ -0,0 +1,36 @@
+package co.aurasphere.facebot.model.outcoming.template.receipt;
+
+import java.io.Serializable;
+
+public class Adjustment implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	
+	private double amount;
+	
+	public Adjustment(){}
+	
+	public Adjustment(String name, double amount){
+		this.name = name;
+		this.amount = amount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+}

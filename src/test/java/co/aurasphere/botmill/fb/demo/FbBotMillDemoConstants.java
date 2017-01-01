817ef7c76cb33b8f046ed5c451aec94eb57c5d62
Alvin Reyes <alvinpermelonareyes@gmail.com>
2017-01-01 15:20:39 -0500

    Added license header

--- src/test/java/co/aurasphere/botmill/fb/demo/FbBotMillDemoConstants.java
+++ src/test/java/co/aurasphere/botmill/fb/demo/FbBotMillDemoConstants.java
@@ -9,12 +9,12 @@ package co.aurasphere.botmill.fb.demo;
 public class FbBotMillDemoConstants {
 
 	/** The Constant PAGE_TOKEN. */
-	public static final String PAGE_TOKEN = "PAGE_TOKEN";
+	public static final String PAGE_TOKEN = "EAAIyxesjRBcBAKO7ARTrKn2ZCIHcLwZC2mWuqwNhhVlqK9XEhsMU1iARYRwAZBj6r6fsvD2NDLtz9FLLki0ZBiUIaPfo7LGAhA09NZCKjJVLvvE70siQO9xwHTpZB5dOFYJIIfycxtpSpU3GePk7ErGToxvApHJGsSUQzYZCJr36QZDZD";
 
 	/** The Constant VALIDATION_TOKEN. */
-	public static final String VALIDATION_TOKEN = "VERIFICATION_CODE";
+	public static final String VALIDATION_TOKEN = "123456789";
 
 	/** The Constant MOCK_FACEBOOK_ID. */
-	public static final String MOCK_FACEBOOK_ID = "FACEBOOK_ID";
+	public static final String MOCK_FACEBOOK_ID = "1158621824216736";
 
 }

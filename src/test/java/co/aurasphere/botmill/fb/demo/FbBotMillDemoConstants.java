3e33d176ebeef53c1d0315b5bd3305ecea43b406
Alvin Reyes <alvinpermelonareyes@gmail.com>
2016-12-31 19:50:27 -0500

    Added back the FbBotMillDemoConstants

---
+++ src/test/java/co/aurasphere/botmill/fb/demo/FbBotMillDemoConstants.java
@@ -0,0 +1,20 @@
+package co.aurasphere.botmill.fb.demo;
+
+
+/**
+ * Put your data here to test the FaceBot framework.
+ * 
+ * @author Donato Rimenti
+ */
+public class FbBotMillDemoConstants {
+
+	/** The Constant PAGE_TOKEN. */
+	public static final String PAGE_TOKEN = "PAGE_TOKEN";
+
+	/** The Constant VALIDATION_TOKEN. */
+	public static final String VALIDATION_TOKEN = "VERIFICATION_CODE";
+
+	/** The Constant MOCK_FACEBOOK_ID. */
+	public static final String MOCK_FACEBOOK_ID = "FACEBOOK_ID";
+
+}

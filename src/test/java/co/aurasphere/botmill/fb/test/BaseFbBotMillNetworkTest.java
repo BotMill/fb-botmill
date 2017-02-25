f2a977c026b38ad034a68b1e11e717da26d6857c
Alvin Reyes <alvinpermelonareyes@gmail.com>
2017-02-24 20:32:22 -0500

    Added a few fields on botmill.properties for testing

--- src/test/java/co/aurasphere/botmill/fb/test/BaseFbBotMillNetworkTest.java
+++ src/test/java/co/aurasphere/botmill/fb/test/BaseFbBotMillNetworkTest.java
@@ -28,6 +28,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import co.aurasphere.botmill.core.internal.util.ConfigurationUtils;
 import co.aurasphere.botmill.fb.AbstractFbBot;
 import co.aurasphere.botmill.fb.FbBotMillContext;
 import co.aurasphere.botmill.fb.support.FbBotMillMockMediator;
@@ -130,6 +132,7 @@ public abstract class BaseFbBotMillNetworkTest extends AbstractFbBot {
 			monitor = new FbBotMillTestMonitor();
 			FbBotMillContext.getInstance().registerMonitor(monitor);
 		}
+		
 	}
 
 	/**
